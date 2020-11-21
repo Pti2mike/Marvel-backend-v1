3a062d9efbba67f5b2e7b8454220351bee14e3c4
MICKAEL DELLON <pti2@macbook-pro-de-mickael.home>
2020-11-21 18:41:31 +0100

    characters route updated

--- routes/characters.js
+++ routes/characters.js
@@ -27,7 +27,7 @@ router.get("/characters", async (req, res) => {
 
     // création de la requete vers l'API Marvel
     const response = await axios.get(
-      `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`
+      `http://gateway.marvel.com/v1/public/characters?limit=100&ts=${ts}&apikey=${publicKey}&hash=${hash}`
     );
     res.json(response.data.data);
   } catch (error) {
